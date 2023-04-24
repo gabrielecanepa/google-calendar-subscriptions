9d4410d23b4c20133ef37a0c09841ae3f413d304
Gabriele Canepa <git@gabrielecanepa.com>
2023-04-24 20:26:56 +0200

    [calendard] add calendars

--- src/calendars.ts
+++ src/calendars.ts
@@ -14,25 +14,34 @@ export type Calendar = GoogleCalendar.Schema$Calendar & {
 const calendars: Calendar[] = [
   {
     name: 'holidays',
-    calendarId: 'YOUR_CALENDAR_ID',
+    calendarId: 'de96d3739c7ccbb31c3c913d9ab09d4a8ea35a6db2d4461893173e3b759aba6b@group.calendar.google.com',
     subscriptionUrl: 'https://officeholidays.com/ics-all/italy',
     fn: (events: CalendarEvent[]): CalendarEvent[] =>
       events.map(event => {
-        const summary = event.summary?.replace('(Not a Public Holiday)', '').trim()
-        const baseDescription = event.description?.split('\n')[ 0 ].trim()
-        const description = baseDescription?.endsWith('.') ? baseDescription : `${baseDescription}.`
+        const summary = event.summary.replace('(Not a Public Holiday)', '').trim()
+        const baseDescription = event.description.split('\n')[ 0 ].trim()
+        const description = baseDescription.endsWith('.') ? baseDescription : `${baseDescription}.`
         return { ...event, summary, description }
       }),
   },
   {
+    name: 'milan',
+    calendarId: '39984c3252baa9ee1a164c8a2e6a7931072ca5f5722c1aab334ee240d2d1bb93@group.calendar.google.com',
+    subscriptionUrl: 'https://skysports.com/calendars/football/fixtures/teams/ac-milan?live=false',
+    fn: (events: CalendarEvent[]): CalendarEvent[] => events.map(event => {
+      const description = event.description?.split('\n').pop().trim() || null
+      const location = event.location ? `${event.location} Stadium` : null
+      return { ...event, description, location }
+    }),
+  },
+  {
     name: 'bulls',
-    calendarId: 'YOUR_CALENDAR_ID',
+    calendarId: '8104efd5db33dcfd2e03fc356612e6fc709678344243063cba7f4834cbc21d2e@group.calendar.google.com',
     subscriptionUrl: 'https://stanza.co/api/schedules/nba-bulls/nba-bulls.ics',
     fn: events => events.map(event => {
       // Prevent event id from changing (stanza.co behavior).
-      const startDate = event.start?.dateTime?.split('T')[0] || ''
-      const id = toBase32Hex(`${event.summary}${startDate}`)
-      const summary = event.summary?.replace('vs.', 'v')
+      const id = toBase32Hex(event.summary + event.start.dateTime.split('T')[0])
+      const summary = event.summary.replace('vs.', 'v')
       return { ...event, id, summary, description: null }
     }),
   },
