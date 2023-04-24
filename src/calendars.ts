4859efce635f772d99221595dee1b14c361fee88
Gabriele Canepa <git@gabrielecanepa.com>
2023-04-24 10:33:38 +0200

    ci: run action only on `calendars` branch

--- src/calendars.ts
+++ src/calendars.ts
@@ -3,7 +3,7 @@ import { calendar_v3 as GoogleCalendar } from '@googleapis/calendar'
 export type CalendarEvent = GoogleCalendar.Schema$Event
 export type CalendarDateTime = GoogleCalendar.Schema$EventDateTime
 
-export type Calendar = {
+export type Calendar = GoogleCalendar.Schema$Calendar & {
   name?: string
   calendarId: string
   subscriptionUrl: string
@@ -12,16 +12,32 @@ export type Calendar = {
 
 const calendars: Calendar[] = [
   {
+    name: 'Holidays in Italy',
+    calendarId: 'de96d3739c7ccbb31c3c913d9ab09d4a8ea35a6db2d4461893173e3b759aba6b@group.calendar.google.com',
+    subscriptionUrl: 'https://officeholidays.com/ics-all/italy',
+    fn: (events: CalendarEvent[]): CalendarEvent[] =>
+      events.map(event => {
+        const summary = event.summary.replace('(Not a Public Holiday)', '').trim()
+        const baseDescription = event.description.split('\n')[ 0 ].trim()
+        const description = baseDescription.endsWith('.') ? baseDescription : `${baseDescription}.`
+        return { ...event, summary, description }
+      }),
+  },
+  {
     name: 'A.C. Milan',
-    calendarId: 'YOUR_CALENDAR_ID',
+    calendarId: '39984c3252baa9ee1a164c8a2e6a7931072ca5f5722c1aab334ee240d2d1bb93@group.calendar.google.com',
     subscriptionUrl: 'https://skysports.com/calendars/football/fixtures/teams/ac-milan?live=false',
     fn: (events: CalendarEvent[]): CalendarEvent[] => events.map(event => {
-      // Use only league in the description.
       const description = event.description?.split('\n').pop().trim() || null
-      // Add stadium to the location to make map apps recognize it.
       const location = event.location ? `${event.location} Stadium` : null
       return { ...event, description, location }
     }),
+  },
+  {
+    name: 'Chicago Bulls',
+    calendarId: '110479dfe01a319ab15b670535140865a4c168622c57a7236fc1137b2c532c8f@group.calendar.google.com',
+    subscriptionUrl: 'https://stanza.co/api/schedules/nba-bulls/nba-bulls.ics',
+    fn: events => events.map(event => ({ ...event, summary: event.summary.replace('vs.', 'v'), description: null })),
   },
 ]
 
