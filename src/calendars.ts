bacb4e934a3bbb9715a84138ffbd0d2c9e1e6d80
Gabriele Canepa <git@gabrielecanepa.com>
2023-05-31 15:01:16 +0200

    [calendars] review subscriptions + global updates

--- src/calendars.ts
+++ src/calendars.ts
@@ -1,34 +1,54 @@
-import { calendar_v3 as GoogleCalendar } from '@googleapis/calendar'
-import { toBase32Hex } from './utils'
+import { calendar_v3 } from '@googleapis/calendar'
+import { stringifyList, toBase32Hex } from './utils'
 
-export type CalendarEvent = GoogleCalendar.Schema$Event
-export type CalendarDateTime = GoogleCalendar.Schema$EventDateTime
+export type CalendarDateTime = {
+  date?: string
+  dateTime?: string
+  timeZone?: string
+}
 
-export type Calendar = GoogleCalendar.Schema$Calendar & {
+export type CalendarEvent = {
+  id: string
+  summary: string
+  location: string | null
+  description: string | null
+  url: string | null
+  start: CalendarDateTime
+  end: CalendarDateTime
+}
+
+export type CalendarSubscription = {
   name: string
   calendarId: string
   subscriptionUrl: string
-  fn?: (events: CalendarEvent[]) => CalendarEvent[]
+  fn?: (events: CalendarEvent[]) => calendar_v3.Schema$Event[]
 }
 
-const calendars: Calendar[] = [
+const subscriptions: CalendarSubscription[] = [
   {
     name: 'holidays',
-    calendarId: 'de96d3739c7ccbb31c3c913d9ab09d4a8ea35a6db2d4461893173e3b759aba6b@group.calendar.google.com',
+    calendarId: '517fd34f8d0dfc12870c861676ba74af633ea5878cbcaa93c704f05d11277141@group.calendar.google.com',
     subscriptionUrl: 'https://officeholidays.com/ics-all/italy',
-    fn: (events: CalendarEvent[]): CalendarEvent[] =>
-      events.map(event => {
-        const summary = event.summary.replace('(Not a Public Holiday)', '').trim()
-        const baseDescription = event.description.split('\n')[ 0 ].trim()
-        const description = baseDescription.endsWith('.') ? baseDescription : `${baseDescription}.`
-        return { ...event, summary, description }
-      }),
+    fn: events =>
+      events.reduce((acc, event) => {
+        const regionalHoliday = event.location && event.location !== 'Italy'
+          ? `(${stringifyList(event.location.split(', '))} only)`
+          : event.location
+        const summary = event.summary
+          .replace('(Not a Public Holiday)', '')
+          .replace('(Regional Holiday)', regionalHoliday)
+          .trim()
+        let description = event.description.split('\n')[0].trim()
+        description = description.endsWith('.') ? description : `${description}.`
+        if (event.url) description += `\n\n${event.url.replace('www.', '')}`
+        return [...acc, { ...event, summary, description }]
+      }, []),
   },
   {
     name: 'milan',
     calendarId: '39984c3252baa9ee1a164c8a2e6a7931072ca5f5722c1aab334ee240d2d1bb93@group.calendar.google.com',
     subscriptionUrl: 'https://skysports.com/calendars/football/fixtures/teams/ac-milan?live=false',
-    fn: (events: CalendarEvent[]): CalendarEvent[] => events.map(event => {
+    fn: events => events.map(event => {
       const description = event.description?.split('\n').pop().trim() || null
       const location = event.location ? `${event.location} Stadium` : null
       return { ...event, description, location }
@@ -36,7 +56,7 @@ const calendars: Calendar[] = [
   },
   {
     name: 'nba',
-    calendarId: '3f2731b24b12b68211adfefb3ada0f5fc4d4843225652d704a067e8d50200443@group.calendar.google.com',
+    calendarId: 'cf69becb29a77f67a250d73e43e406915f9bfef0eb8a389da9d8f78864c8092f@group.calendar.google.com',
     subscriptionUrl: 'https://stanza.co/api/schedules/nba-bulls/nba-bulls.ics',
     fn: events => events.map(e => {
       // Prevent event id from changing (stanza.co behavior).
@@ -50,4 +70,4 @@ const calendars: Calendar[] = [
   },
 ]
 
-export default calendars
+export default subscriptions
--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,25 @@
 export const BASE32HEX_REGEXP = /([a-v]|[0-9])/gi
 export const DATE_REGEXP = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/i
 
-export const toBase32Hex = (s: string): string => s.match(BASE32HEX_REGEXP)?.join('').toLowerCase() || ''
+export const toBase32Hex = (string: string): string => string.match(BASE32HEX_REGEXP)?.join('').toLowerCase() || ''
 
-export const isDate = (s: string): boolean => DATE_REGEXP.test(s)
+export const isDate = (string: string): boolean => DATE_REGEXP.test(string)
+
+export type StringifyListOptions = {
+  style: 'short' | 'long',
+  type: 'conjunction' | 'disjunction'
+}
+
+const DEFAULT_STRINGIFY_LIST_OPTIONS: StringifyListOptions = {
+  style: 'short',
+  type: 'conjunction',
+}
+
+/**
+ * Returns a string with the elements of the list formatted according to the options.
+ */
+export const stringifyList = (list: any[], options: StringifyListOptions = DEFAULT_STRINGIFY_LIST_OPTIONS): string => {
+  const comma = options.style === 'short' ? '' : ','
+  const connector = options.type === 'conjunction' ? 'and' : 'or'
+  return list.join(', ').replace(/, ([^,]*)$/, `${comma} ${connector} $1`)
+}
