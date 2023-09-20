39f65a4585ed2068337a1f9e700df3499de23f25
Gabriele Canepa <git@gabrielecanepa.com>
2023-09-20 22:11:22 +0200

    refactor: general refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import { config as configEnv } from 'dotenv'
 configEnv()
 
-import { auth as googleAuth, calendar as googleCalendar, calendar_v3 } from '@googleapis/calendar'
-import { Component as ICalComponent, Event as ICalEvent, Timezone as ICalTimezone, parse } from 'ical.js'
+import { auth as googleAuth, calendar as googleCalendar } from '@googleapis/calendar'
 import calendars, { CalendarDateTime, CalendarEvent, CalendarSubscription } from './calendars'
-import { isDate, toBase32Hex } from './utils'
+import { isEqual, parseIcs, toBase32Hex } from './utils'
 
 const GOOGLE_CLIENT_EMAIL = process.env.GOOGLE_CLIENT_EMAIL
 const GOOGLE_PRIVATE_KEY = process.env.GOOGLE_PRIVATE_KEY
@@ -26,56 +25,21 @@ const auth = new googleAuth.GoogleAuth({
 const client = googleCalendar({ version: 'v3', auth })
 
 /**
- * Extracts dates from an iCal component and format them according to the Google Calendar API.
- *
- * @url https://developers.google.com/calendar/api/v3/reference/events#resource
- */
-// eslint-disable-next-line
-const getDatesFromICalComponent = (component: ICalComponent, timeZone: string): { start: CalendarDateTime, end: CalendarDateTime } => {
-  return ['start', 'end'].reduce((acc, key) => {
-    const [, { tzid },, dateTime] = component.getFirstProperty(`dt${key}`).toJSON()
-    if (isDate(dateTime)) return { ...acc, [key]: { date: dateTime } }
-    return { ...acc, [key]: { dateTime, timeZone: tzid || timeZone } }
-  }, { start: { date: null }, end: { date: null } })
-}
-
-/**
- * Format an iCal component as a Google Calendar resource.
- *
- * @url https://developers.google.com/calendar/api/v3/reference/events#resource
- */
-const parseIcs = (ics: string): CalendarEvent[] => {
-  const iCal = new ICalComponent(parse(ics))
-  const { tzid } = new ICalTimezone(iCal.getFirstSubcomponent('vtimezone'))
-
-  return iCal.getAllSubcomponents('vevent').map(event => {
-    const { uid, summary, location, description } = new ICalEvent(event)
-    const url = event.getFirstPropertyValue('url') || null
-    return { id: toBase32Hex(uid), summary, location, description, url, ...getDatesFromICalComponent(event, tzid) }
-  })
-}
-
-/**
- * Check if a calendar event from an iCal or Google is equal to another.
- */
-const isEqual = (a: CalendarEvent | calendar_v3.Schema$Event, b: CalendarEvent | calendar_v3.Schema$Event): boolean => (
-  a.summary === b.summary &&
-  a.location === b.location &&
-  a.description === b.description &&
-  JSON.stringify(a.start) === JSON.stringify(b.start) &&
-  JSON.stringify(a.end) === JSON.stringify(b.end)
-)
-
-/**
  * Sync a calendar.
  */
-export const syncSubscription = async (subscription: CalendarSubscription): Promise<void> => {
+export const syncSubscription = async (subscription: CalendarSubscription, options: { clear?: boolean } = {}): Promise<void> => {
   const { calendarId, subscriptionUrl, fn = (events: CalendarEvent[]): CalendarEvent[] => events } = subscription
 
   try {
     const googleEvents = await (await client.events.list({ calendarId })).data.items || []
     const ics = await (await fetch(subscriptionUrl)).text()
     const events = parseIcs(ics)
+
+    if (options.clear) {
+      for (const event of googleEvents) {
+        await client.events.delete({ calendarId, eventId: event.id })
+      }
+    }
 
     for (const event of fn(events)) {
       // Find the original event in the calendar.
@@ -101,7 +65,7 @@ export const syncSubscription = async (subscription: CalendarSubscription): Prom
  */
 export const syncSubscriptions = async (subscriptions: CalendarSubscription[]): Promise<void> => {
   for (const subscription of subscriptions) {
-    await syncSubscription(subscription)
+    await syncSubscription(subscription, { clear: true })
   }
 }
 
@@ -113,6 +77,6 @@ export {
   toBase32Hex,
 }
 
-// Sync all calendars specified in env.
+// Sync all calendars specified in the env.
 const activeCalendars = calendars.filter(calendar => SUBSCRIPTIONS.includes(calendar.name))
 syncSubscriptions(activeCalendars)
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,22 @@
+import { calendar_v3 } from '@googleapis/calendar'
+import { Component as ICalComponent, Event as ICalEvent, Timezone as ICalTimezone, parse } from 'ical.js'
+import { CalendarDateTime, CalendarEvent } from './calendars'
+
+// Environment
+
 export const BASE32HEX_REGEXP = /([a-v]|[0-9])/gi
 export const DATE_REGEXP = /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/i
 
+// Utilities
+
+/**
+ * Converts a string to base32hex format.
+ */
 export const toBase32Hex = (string: string): string => string.match(BASE32HEX_REGEXP)?.join('').toLowerCase() || ''
 
+/**
+ * Checks if a string is a date in the format YYYY-MM-DD.
+ */
 export const isDate = (string: string): boolean => DATE_REGEXP.test(string)
 
 export type StringifyListOptions = {
@@ -23,3 +37,46 @@ export const stringifyList = (list: any[], options: StringifyListOptions = DEFAU
   const connector = options.type === 'conjunction' ? 'and' : 'or'
   return list.join(', ').replace(/, ([^,]*)$/, `${comma} ${connector} $1`)
 }
+
+// Calendar
+
+/**
+ * Extracts dates from an iCal component and format them according to the Google Calendar API.
+ *
+ * @url https://developers.google.com/calendar/api/v3/reference/events#resource
+ */
+// eslint-disable-next-line
+export const getDatesFromICalComponent = (component: ICalComponent, timeZone: string): { start: CalendarDateTime, end: CalendarDateTime } => {
+  return ['start', 'end'].reduce((acc, key) => {
+    const [, { tzid },, dateTime] = component.getFirstProperty(`dt${key}`).toJSON()
+    if (isDate(dateTime)) return { ...acc, [key]: { date: dateTime } }
+    return { ...acc, [key]: { dateTime, timeZone: tzid || timeZone } }
+  }, { start: { date: null }, end: { date: null } })
+}
+
+/**
+ * Format an iCal component as a Google Calendar resource.
+ *
+ * @url https://developers.google.com/calendar/api/v3/reference/events#resource
+ */
+export const parseIcs = (ics: string): CalendarEvent[] => {
+  const iCal = new ICalComponent(parse(ics))
+  const { tzid } = new ICalTimezone(iCal.getFirstSubcomponent('vtimezone'))
+
+  return iCal.getAllSubcomponents('vevent').map(event => {
+    const { uid, summary, location, description } = new ICalEvent(event)
+    const url = event.getFirstPropertyValue('url') || null
+    return { id: toBase32Hex(uid), summary, location, description, url, ...getDatesFromICalComponent(event, tzid) }
+  })
+}
+
+/**
+ * Check if a calendar event from an iCal or Google is equal to another.
+ */
+export const isEqual = (a: CalendarEvent | calendar_v3.Schema$Event, b: CalendarEvent | calendar_v3.Schema$Event): boolean => (
+  a.summary === b.summary &&
+  a.location === b.location &&
+  a.description === b.description &&
+  JSON.stringify(a.start) === JSON.stringify(b.start) &&
+  JSON.stringify(a.end) === JSON.stringify(b.end)
+)
